669ad063cd4c88d9f065986fa995fb0f07bf8202
st2eve <houbart.steeve@gmail.com>
2022-07-07 14:09:45 +0200

    fin

---
+++ build/precache-manifest.de8fad431c56796f4e968f73fd2bda3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "21ea4781241389601d5a",
+    "url": "/static/css/main.d3e202c2.chunk.css"
+  },
+  {
+    "revision": "21ea4781241389601d5a",
+    "url": "/static/js/main.4102ae4a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a71e82d061cc15d59419",
+    "url": "/static/js/2.f7927bc3.chunk.js"
+  },
+  {
+    "revision": "3875be00d6c71d6058b30e55865171fc",
+    "url": "/index.html"
+  }
+];
